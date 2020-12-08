f950f87a1d67373e4ad2049357019880b5245f0f
Damiano <damicodamiano10@gmail.com>
2020-12-08 20:18:57 +0100

    Close #3

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -14,14 +14,33 @@ import it.unipd.tos.model.MenuItem;
 
 public class TakeAwayBillImpl implements TakeAwayBill {
 
-    @Override
+    
     public double getOrderPrice(List<MenuItem> itemsOrdered, User user, LocalTime time) throws RestaurantBillException {
 
         double price = 0;
+        int n_gelati = 0;
+        double min_price_gelati = 0;
+        boolean first = false;
+        
        
         for (int i = 0; i < itemsOrdered.size(); i++) {
             
+            if (itemsOrdered.get(i).getType() == itemType.Gelati) {
+                n_gelati++;
+                if (!first) {
+                    min_price_gelati = itemsOrdered.get(i).getPrice();
+                    first = true;
+                } else if (min_price_gelati > itemsOrdered.get(i).getPrice()) {
+                    min_price_gelati = itemsOrdered.get(i).getPrice();
+                }
+                    
+            }
+            
             price += itemsOrdered.get(i).getPrice();
+        }
+        
+        if (n_gelati > 5) {
+            price -= (min_price_gelati / 2);
         }
         
         return price;
--- src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
@@ -13,6 +13,25 @@ import org.junit.Test;
 
 public class TakeAwayBillTest {
 
-	
+    @Test
+    public void sconto_5() {
+        TakeAwayBillImpl bill=new TakeAwayBillImpl();
+        User u=new User(1,"Giovanni",LocalDate.of(1999, 5, 3));
+        List<MenuItem> ord=new ArrayList<MenuItem>();
+        ord.add(new MenuItem(itemType.Gelati,"Fragola",2));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
+        ord.add(new MenuItem(itemType.Budini,"Caramello",2));
+        ord.add(new MenuItem(itemType.Bevande,"Fanta",2));
+        try {
+            assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26)),20.0,0.0001);
+        }
+        catch(RestaurantBillException e){
+            fail();
+        }
+    }
 	
 }
