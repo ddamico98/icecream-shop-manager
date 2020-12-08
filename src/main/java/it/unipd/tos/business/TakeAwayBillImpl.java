d3f5829d7d1cebf21f44357b5dd76a1dc4b0e804
Damiano <damicodamiano10@gmail.com>
2020-12-08 20:38:38 +0100

    Close #4

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -14,17 +14,21 @@ import it.unipd.tos.model.MenuItem;
 
 public class TakeAwayBillImpl implements TakeAwayBill {
 
-    
+    @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered, User user, LocalTime time) throws RestaurantBillException {
 
         double price = 0;
         int n_gelati = 0;
+        double tot_bud_gel = 0;
         double min_price_gelati = 0;
         boolean first = false;
         
        
         for (int i = 0; i < itemsOrdered.size(); i++) {
-            
+            if (itemsOrdered.get(i).getType() == itemType.Gelati || itemsOrdered.get(i).getType() == itemType.Budini)
+            { 
+             tot_bud_gel += itemsOrdered.get(i).getPrice(); 
+            }
             if (itemsOrdered.get(i).getType() == itemType.Gelati) {
                 n_gelati++;
                 if (!first) {
@@ -41,6 +45,11 @@ public class TakeAwayBillImpl implements TakeAwayBill {
         
         if (n_gelati > 5) {
             price -= (min_price_gelati / 2);
+            tot_bud_gel -= (min_price_gelati / 2);
+        }
+        
+        if (tot_bud_gel > 50) {
+            price -= price * 0.1;
         }
         
         return price;
--- src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
@@ -16,7 +16,7 @@ public class TakeAwayBillTest {
     @Test
     public void sconto_5() {
         TakeAwayBillImpl bill=new TakeAwayBillImpl();
-        User u=new User(1,"Giovanni",LocalDate.of(1999, 5, 3));
+        User u=new User(1,"Giovanni",LocalDate.of(1998, 7, 28));
         List<MenuItem> ord=new ArrayList<MenuItem>();
         ord.add(new MenuItem(itemType.Gelati,"Fragola",2));
         ord.add(new MenuItem(itemType.Gelati,"Nocciola",3));
@@ -28,6 +28,24 @@ public class TakeAwayBillTest {
         ord.add(new MenuItem(itemType.Bevande,"Fanta",2));
         try {
             assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26)),20.0,0.0001);
+        }
+        catch(RestaurantBillException e){
+            fail();
+        }
+    }
+    
+    @Test
+    public void sconto_10pc_superati_50_euro(){
+        TakeAwayBillImpl bill=new TakeAwayBillImpl();
+        User u=new User(1,"Giovanni",LocalDate.of(1998, 7, 28));
+        List<MenuItem> ord=new ArrayList<MenuItem>();
+        ord.add(new MenuItem(itemType.Gelati,"Fragola",5));
+        ord.add(new MenuItem(itemType.Budini,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Budini,"Pistacchio",30));
+        ord.add(new MenuItem(itemType.Gelati,"Mandorla",15));
+        ord.add(new MenuItem(itemType.Gelati,"caffè",10));
+        try {
+            assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26)), 63.0 ,0.0001);
         }
         catch(RestaurantBillException e){
             fail();
