847419a82ec4adf62aa745ff03a6585b44a026b0
Damiano <damicodamiano10@gmail.com>
2020-12-08 22:08:20 +0100

    Close #7

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -13,8 +13,8 @@ import it.unipd.tos.model.User;
 import it.unipd.tos.model.MenuItem;
 
 public class TakeAwayBillImpl implements TakeAwayBill {
-
-    @Override
+    private List<User> free_ords = new ArrayList<User>();
+   @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered, User user, LocalTime time) throws RestaurantBillException {
 
         double price = 0;
@@ -56,6 +56,24 @@ public class TakeAwayBillImpl implements TakeAwayBill {
         
         if (price < 10) {
             price += 0.5;
+        }
+        
+        Period under = Period.between(user.getDate(), LocalDate.now());
+        if (under.getYears() < 18 && time.isAfter(LocalTime.of(18, 0, 0)) && time.isBefore(LocalTime.of(19, 0, 0))
+                && free_ords.size() < 10) {
+            boolean check = false;
+            int rand = 0;
+            for (int i = 0; i < free_ords.size(); i++) {
+                if (free_ords.get(i).getId() == user.getId()) {
+                    check = true;
+                }
+                    
+            }
+            rand = time.getSecond();
+            if (rand % 2 == 0 && !check) {
+                price = 0;
+                free_ords.add(user);
+            }
         }
         
         return price;
--- src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayBillTest.java
@@ -51,6 +51,26 @@ public class TakeAwayBillTest {
             fail();
         }
     }
+    @Test
+    public void sconto_10pc_senza_sconto_50pc(){
+        //in questo caso viene effettuato uno sconto del 10% facendo scendere il prezzo sotto i 50 €
+        //e non viene preso il secondo sconto del 50 % sul prezzo più basso
+        TakeAwayBillImpl bill=new TakeAwayBillImpl();
+        User u=new User(1,"Tommaso",LocalDate.of(1999, 5, 3));
+        List<MenuItem> ord=new ArrayList<MenuItem>();
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",10));
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",10));
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",10));
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",10));
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",6));
+        ord.add(new MenuItem(itemType.Gelati,"Pistacchio",5));
+        try {
+            assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26)), 48.5 ,0.0001);
+        }
+        catch(RestaurantBillException e){
+            fail();
+        }
+    }
     
     @Test
     public void sconto_10pc_e_sconto_superati_5_gelati(){
@@ -154,6 +174,75 @@ public class TakeAwayBillTest {
         ord.add(new MenuItem(itemType.Gelati,"Nocciola",1));
         try {
             assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26)), 7.5 ,0.0001);
+        }
+        catch(RestaurantBillException e){
+            fail();
+        }
+    }
+    
+    
+    @Test
+    public void scontoMinorenni(){
+        TakeAwayBillImpl bill=new TakeAwayBillImpl();
+        User u=new User(1,"Giovanni",LocalDate.of(2004, 5, 4));
+        List<MenuItem> ord=new ArrayList<MenuItem>();
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",5));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",6));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        try {
+            //sconto minorenni accettato
+            assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26,24)), 0.00 ,0.0001);
+            //sconto minorenni rifiutato
+            assertEquals(bill.getOrderPrice(ord,u,LocalTime.of(18,26,25)), 48.5 ,0.0001);
+
+        }
+        catch(RestaurantBillException e){
+            fail();
+        }
+    }
+    
+    @Test
+    public void max_numero_sconto_minorenni(){
+        TakeAwayBillImpl bill=new TakeAwayBillImpl();
+        List<User> utenti = new ArrayList<User>();
+        for(int i = 0; i<13;i++) {
+            utenti.add(new User(i,"Giovanni",LocalDate.of(2004, 10, 5)));
+        }
+        utenti.add(new User(13,"Giovanni",LocalDate.of(1998, 7, 28)));
+
+
+        List<MenuItem> ord=new ArrayList<MenuItem>();
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",5));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",6));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        ord.add(new MenuItem(itemType.Gelati,"Nocciola",10));
+        try {
+            //sconto minorenni accettato
+            assertEquals(bill.getOrderPrice(ord,utenti.get(0),LocalTime.of(18,26,24)), 0.00 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(13),LocalTime.of(18,26,24)), 48.5 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(13),LocalTime.of(16,26,24)), 48.5 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(13),LocalTime.of(20,26,24)), 48.5 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(0),LocalTime.of(17,26,24)), 48.5 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(0),LocalTime.of(21,26,24)), 48.5 ,0.0001);
+            for(int i =0; i<10;i++) {
+                if(i==0) {
+                    assertEquals(bill.getOrderPrice(ord,utenti.get(i),LocalTime.of(18,26,22)), 48.5 ,0.0001);
+                }else {
+                    assertEquals(bill.getOrderPrice(ord,utenti.get(i),LocalTime.of(18,26,22)), 0.00 ,0.0001);
+                }
+            }
+
+            assertEquals(bill.getOrderPrice(ord,utenti.get(10),LocalTime.of(18,26,25)), 48.5 ,0.0001);
+            assertEquals(bill.getOrderPrice(ord,utenti.get(11),LocalTime.of(18,26,29)), 48.5 ,0.0001);
+            // Sono stati superati i minorenni con sconto
+            assertEquals(bill.getOrderPrice(ord,utenti.get(12),LocalTime.of(18,26,22)), 48.5 ,0.0001);
+
+
         }
         catch(RestaurantBillException e){
             fail();
